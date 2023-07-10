c24a550ae2802ab217be1af90fadad0714ef44e7
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2023-07-10 19:54:20 +0300

    firevase structure

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import { AuthProvider } from "./Auth";
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<SignUp />} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
